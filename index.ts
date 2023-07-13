90d069793e522f9d8cd39d61f4d0f03fc1954ced
gantonette <62356789+gantonette@users.noreply.github.com>
2023-07-13 20:21:20 +1200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+// Create a book table
+const booksTable = new aws.dynamodb.Table("booksTable", {
+    attributes: [{
+        name: "id",
+        type: "S",
+    }],
+    hashKey: "id",
+    billingMode: "PAY_PER_REQUEST",
+});
+
+// Create a book entries table
+const bookEntriesTable = new aws.dynamodb.Table("bookEntriesTable", {
+    attributes: [{
+        name: "entryId",
+        type: "S",
+    }],
+    hashKey: "entryId",
+    billingMode: "PAY_PER_REQUEST",
+});
+
+// Create an S3 bucket
+const bookFilesBucket = new aws.s3.Bucket("bookFilesBucket");
+
+// Export the names of the resources
+export const booksTableName = booksTable.name;
+export const bookEntriesTableName = bookEntriesTable.name;
+export const bookFilesBucketName = bookFilesBucket.id;
